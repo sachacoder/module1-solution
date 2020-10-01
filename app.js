0fda66024647991c30bb802289ca3d3ecd2c1d54
Sach <sachinwasmatkar@gmail.com>
2020-10-02 00:23:57 +0530

    updated web files

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+    angular.module('FoodItemCounterApp', [])
+        .controller('FoodItemCounterCtrl', function ($scope) {
+            $scope.foodItems = "";
+            $scope.foodItemsCount = 0;
+            $scope.message = "Check If Too Much";
+            $scope.calculateFoodItems = function () {
+                $scope.foodItemsCount = calculateItems($scope.foodItems);
+            };
+
+            function calculateItems(_foodItems) {
+                if(_foodItems ==="") {
+                    $scope.message = "Please enter data first";
+                }else {
+                    var totalItems = 0;
+                    var foodItemArray = _foodItems.split(",");
+                    totalItems = foodItemArray.length;
+                    $scope.foodItemsCount = totalItems
+                    if(totalItems<=3) {
+                        $scope.message = "Enjoy!";
+                    }else {
+                        $scope.message = "Too much!";
+                    }
+                }
+
+            }
+        });
+})();
